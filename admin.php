ad956b4296acd34164e2ba7be4abb371f941b695
Melanie Methe <meth0020@algonquinlive.com>
2022-11-28 17:54:10 -0500

    Changed to PHP

---
+++ admin.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Melanie, Kristina, Banu">
+    <title>Admin Page</title>
+</head>
+<body>
+    <h1>Admin Page</h1> 
+    <!-- video PHP crud app watch (shows how to use XAMPP) on Youtube -->
+</body>
+</html>
