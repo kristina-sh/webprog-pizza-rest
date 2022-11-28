4f7857d4072260c0edaee94f176e4d0305850f11
Kristina Shalaginova <kristina.shalaginovaa@gmail.com>
2022-11-28 15:03:56 -0500

    adding filter function

--- JS/menu.js
+++ JS/menu.js
@@ -2,11 +2,12 @@
 //Assignment 2
 //Students: Kristina Shalaginova, Melanie Methe, Banumajan Mohammad
 
-
-
 //Creating a search function on the menu page
+
+
 function search_item() {
-    let input = document.getElementById('searchbar').value
+
+    let input = document.getElementById('searchbar').value;
     input = input.toLowerCase();
     let x = document.getElementsByClassName('item');
 
@@ -21,4 +22,21 @@ function search_item() {
 }
 
 //Creatin a filter function on the menu page
+function filter_item() {
+    let y = document.getElementsByClassName('item');
+    var select = document.getElementById('category-select');
+    var value = select.options[select.selectedIndex].value;
 
+    for (i = 0; i < y.length; i++) {
+
+        if (value === "select_item") {
+            y[i].style.display = "list-item";
+        }
+        else if (!y[i].innerHTML.toLowerCase().includes(value)) {
+            y[i].style.display = "none";
+        }
+        else {
+            y[i].style.display = "list-item";
+        }
+    }
+}
