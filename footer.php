f5e5631122a18e3272814bbfc6db34a474569f45
Banu Mohammad <moha0823@algonquinlive.com>
2022-12-04 18:18:31 -0500

    added clear button, return to login,also comments

--- footer.php
+++ footer.php
@@ -1,11 +1,16 @@
+<!--    Course name: Web Programming (CST_8285_312)
+        Assignment 2
+        Student: Banumajan Mohammad 
+-->
 <!DOCTYPE html>
 <html>
 <head>
-<meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
-<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-<meta name="HandheldFriendly" content="true">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link href="css/assignment2.css" rel="stylesheet" type="text/css">
+  <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <meta name="HandheldFriendly" content="true">
+  <meta name="author" content="Banumajan Mohammad">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="css/assignment2.css" rel="stylesheet" type="text/css">
 
 </head>
 <body>
